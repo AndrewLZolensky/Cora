369da27f4e332b8a99ba538ea0678b26d1574d0d
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-10-04 17:54:35 -0400

    implemented backward chaining in horn_clauses.py

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Cognitive Operations and Reasoning Assistant
 
 # Current Capabilities
-Cora can perform forward chaining from Horn clauses, and construct concept lattices from formal contexts.
+Cora can perform forward and backward chaining from Horn clauses, and construct concept lattices from formal contexts.
 
 # Planned Capabilities
-Cora will be given a global ontology, relational concept analysis capabilities, and the ability to perform backward-chaining.
+Cora will be given a global ontology, relational concept analysis capabilities, and causal inference capabilities.
--- horn_clauses.py
+++ horn_clauses.py
@@ -53,7 +53,7 @@ class Reasoner():
             temp += "\n" + fact.__str__()
         return temp + "\n" + "-"*80 + "\n"
     
-    def query_in_knowledge_base(self, query: Union[AtomicProposition, Conjunction]) -> bool:
+    def query_satisfied_by_knowledge_base(self, query: Union[AtomicProposition, Conjunction]) -> bool:
         if isinstance(query, AtomicProposition):
             for atom in self.knowledge_base:
                 if query == atom:
@@ -75,37 +75,54 @@ class Reasoner():
         num_cycles = 0
         num_new_facts = 0
         while True:
-            if (self.query_in_knowledge_base(query=query)):
-                print(f"process completed in {num_cycles} cycles")
-                print(f"added {num_new_facts} facts")
+            if (self.query_satisfied_by_knowledge_base(query=query)):
+                #print(f"process completed in {num_cycles} cycles")
+                #print(f"added {num_new_facts} facts")
                 return True
             num_cycles += 1
             initial_num_facts = len(self.knowledge_base)
             for implication in working_implications.copy():
                 thing_to_satisfy = implication.antecedent
-                satisfied = self.query_in_knowledge_base(thing_to_satisfy)
+                satisfied = self.query_satisfied_by_knowledge_base(thing_to_satisfy)
                 if satisfied:
                     self.knowledge_base.add(implication.consequent)
                     working_implications.remove(implication)
-                    print(f"cycle {num_cycles} added {implication.consequent}")
+                    #print(f"cycle {num_cycles} added {implication.consequent}")
                     num_new_facts += 1
             if (len(self.knowledge_base) == initial_num_facts):
                 break
-        print(f"process completed in {num_cycles} cycles")
-        print(f"added {num_new_facts} facts")
+        #print(f"process completed in {num_cycles} cycles")
+        #print(f"added {num_new_facts} facts")
         return False
     
-    def backward_chain(self, query: AtomicProposition) -> bool:
-        pass
+    def backward_chain(self, query: Union[AtomicProposition, Conjunction]):
+        
+        if (self.query_satisfied_by_knowledge_base(query=query)):
+                return True
+        if isinstance(query, AtomicProposition):
+            for implication in self.implications:
+                if (implication.consequent == query):
+                    if self.backward_chain(implication.antecedent):
+                        return True
+        elif isinstance(query, Conjunction):
+            for implication in self.implications:
+                if (implication.consequent == query.left_conjunct):
+                    if self.backward_chain(implication.antecedent) and self.backward_chain(query.right_conjunct):
+                        return True
+                elif (implication.consequent == query.right_conjunct):
+                    if self.backward_chain(implication.antecedent) and self.backward_chain(query.left_conjunct):
+                        return True
+                    
+        return False
      
 
 if __name__ == "__main__":                   
     # propositions
-    propA = "timmy is hungry"
-    propB = "timmy is angry"
-    propC = "timmy is stressed"
-    propD = "timmy throws a tantrum"
-    propE = "timmy is sorry"
+    propA = "timmy is cool"
+    propB = "timmy is friendly"
+    propC = "timmy is motivated"
+    propD = "timmy is successful"
+    propE = "timmy is happy"
 
     # atomize
     atomA = AtomicProposition(proposition=propA)
@@ -127,7 +144,4 @@ if __name__ == "__main__":
 
     # create reasoner to prove query
     logos = Reasoner(implications=implications, initial_knowledge_base=initial_knowledge_base)
-    # print(logos)
-    # print(logos.forward_chain(query))
-
-    print(logos.forward_chain(AtomicProposition(proposition="jerry is healthy")))
+    print(logos.backward_chain(atomE))
